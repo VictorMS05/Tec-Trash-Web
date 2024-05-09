fd88c09eae18c3b4271d20397cedd1caa240c7bc
ferRMar <145933525+ferRMar@users.noreply.github.com>
2024-05-09 00:07:16 -0600

    corregí diseño de inicio de sesión

--- src/components/login/contenedorCampos.jsx
+++ src/components/login/contenedorCampos.jsx
@@ -6,6 +6,8 @@ import logo_google from "./../../images/logo_google_login.png"
 import logo_x from "./../../images/logo_x_login.png"
 import ojito from "./../../images/icon_mostrarcontra_login.png"
 import { useState } from "react";
+import { Link } from 'react-router-dom';
+import { Link as Scroll } from 'react-scroll';
 
 
 
@@ -63,28 +65,36 @@ export function ContenedorLogin(props){
 
   }
 
-
+  
     
 
     return(
       <div className="contenedor">
         <h3>{props.titulo}</h3>
         <form onSubmit={ingresar} >
-          <div className="campos-ingresos" id="login-form">
+          <div className="campos-ingresos">
             <CamposRegistro  type="text" nombreCampo = "Correo electrónico"  urlImg = {correito} nombre = "correito" id="correito" evento = {(e) => setEmail(e.target.value)}/>
             <CamposRegistro tipo="password" nombreCampo = "Contraseña" urlImg = {candadito} urlImg2 = {ojito}  idOjito = "mostrar_contra" nombre = "password" id="password" evento = {(e) => setContra(e.target.value)}/>
+
+            <p id = "contraIncorrecta" className="contraIncorrecta esconder">Contraseña incorrecta</p>
+            <p className="error esconder">Error al iniciar sesión</p>
+            <Link to='/login' className="avisos-texto">¿Olvidaste tu contraseña?</Link>
+            <button type="submit">Ingresar</button>
           </div>
-          <p id = "contraIncorrecta" className="contraIncorrecta esconder">Contraseña incorrecta</p>
-          <p className="error esconder">Error al iniciar sesión</p>
-          <a href="#">¿Olvidaste tu contraseña?</a>
-          <button type="submit">Ingresar</button>
+          
           <div className='redesSociales'>
             <a href='https://www.google.com/webhp?hl=es&sa=X&ved=0ahUKEwjppZX4triFAxXVIUQIHarcBdUQPAgJ'><img className = "redes" src={logo_google}/></a>
             <a href='https://www.facebook.com/?locale=es_LA'><img className = "redes" src={logo_facebook}/></a>
             <a href='https://twitter.com/?lang=es'><img className = "redes" src={logo_x}/></a>
           </div>
-          <label className="preguntarCuenta">¿No tienes una cuenta?</label>
-          <a href="#" className="crear-cuenta">Crear una</a>
+          <div id="cuenta-texto">
+            <label className="avisos-texto" >¿No tienes una cuenta?</label>
+            <Link className="crear-cuenta avisos-texto">Crear una</Link><br/>
+            <label className="avisos-texto">¿Tienes una empresa?</label>
+            <Link className="crear-cuenta avisos-texto">Inicia sesión</Link>
+          </div>
+          
+          
           
         </form>
     </div>
