201461876fc2d430b2570a4f9c9cb7e051012d4a
VictorMS05 <135737262+VictorMS05@users.noreply.github.com>
2024-05-11 16:18:10 -0600

    reorganice estilos css en una carpeta, agregue imports en App.js y cree componente registro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+// import './App.css';
+import './css/cliente.css';
+import './css/empleado.css';
+import './css/empresa.css';
+import './css/general.css';
+import './css/login.css';
+import './css/pagina_principal.css';
+import './css/registro.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Login } from './components/login';
-import { Principal } from './components/principal';
 import { Cliente } from './components/cliente';
 import { Empleado } from './components/empleado';
 import { Empresa } from './components/empresa';
+import { Login } from './components/login';
+import { Principal } from './components/principal';
+import { Registro } from './components/registro';
 
 function App() { // Este es el componente principal de la aplicación
   return ( // Se regresa un JSX
@@ -14,7 +22,7 @@ function App() { // Este es el componente principal de la aplicación
         {/* Se define la ruta de la página principal y se renderiza*/}
         <Route path="/" element={<Principal />} />
         <Route path="/login_cliente" element={<Login />} />
-        <Route path="/registro_cliente" element={<Login />} />
+        <Route path="/registro_cliente" element={<Registro />} />
         <Route path="/cliente" element={<Cliente />} />
         <Route path="/empleado" element={<Empleado />} />
         <Route path="/empresa" element={<Empresa />} />
